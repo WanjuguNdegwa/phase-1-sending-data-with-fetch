b54f20a0fa174ec2fa904d572f8fb0f0f522c14f
WanjuguNdegwa <99987352+WanjuguNdegwa@users.noreply.github.com>
2022-04-18 21:23:45 +0300

    finish lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(name, email) {
+    const data = { name, email };
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify(data),
+      };
+
+
+    return fetch('http://localhost:3000/users', configurationObject)
+      .then(response => response.json())
+      .then(data => {
+        document.body.append(data.id)
+      })
+      .catch(error => {
+        document.body.append(error.message)
+      }) 
+}
